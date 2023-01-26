1250ba199dcf252feaf4d6f39bff073b8fe18ed4
SC <61970329+sci205@users.noreply.github.com>
2023-01-26 15:42:18 +0100

    name change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='latent_diffusion',
+    name='latentdiffusion',
     version='0.0.2',
     description='',
     packages=find_packages(),
